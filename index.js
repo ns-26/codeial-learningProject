9feee4e413a744facdffcc23a9e8ffb17edc1743
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-03 23:13:37 +0530

    Structure ready

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 //use express router
 app.use('/', require('./routes'));
 
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//port config
 app.listen(port, function(err) {
 	if (err) {
 		console.log(`Error in running the server:${err}`);
