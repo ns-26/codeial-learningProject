abf9a3af7ac5e70c4dbc57926898bfa94fe6c03f
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-08 16:48:43 +0530

    add sass setup

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport_local_stratergy');
 const mongoStore = require('connect-mongo')(session); //storing the session where the connect needs to work
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(
+	sassMiddleware({
+		src: './assets/scss',
+		dest: './assets/css',
+		debug: true,
+		outputStyle: 'extended',
+		prefix: '/css'
+	})
+);
 
 //form controller
 app.use(express.urlencoded({ extended: true }));
