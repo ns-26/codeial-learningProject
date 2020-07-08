a98fe23ac9824fab4a0384194183c96567aa8501
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-08 23:16:10 +0530

    add code for populating the users data with respective posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,22 @@
+const Post = require('../modals/post');
+
 module.exports.home = function(req, res) {
 	// console.log(req.cookies);
-	res.cookie('user_id', 25);
-	console.log(res.cookie);
-	return res.render('home', {
-		title: 'Home Page'
+	// res.cookie('user_id', 25);
+	// console.log(res.cookie);
+
+	// Post.find({}, function(err, posts) {
+	// 	return res.render('home', {
+	// 		title: 'Codeial | Home',
+	// 		posts: posts
+	// 	});
+	// });
+
+	//populate the comments of the entire user
+	Post.find({}).populate('user').exec(function(err, posts) {
+		return res.render('home', {
+			title: 'Codeial | Home',
+			posts: posts
+		});
 	});
 };
