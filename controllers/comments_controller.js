50d3c46821f55d2eea680dc5df94f8737ea13003
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-11 19:17:14 +0530

    add code to delete comments and posts

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -26,3 +26,39 @@ module.exports.create = function(req, res) {
 		}
 	});
 };
+
+module.exports.destroy = function(req, res) {
+	Comment.findById(req.params.id, function(err, comment) {
+		let postId = comment.post;
+		if (err) {
+			console.log('comment does not exist');
+			return res.redirect('back');
+		} else if (req.user.id == comment.user) {
+			comment.remove();
+			Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, function(err, post) {
+				if (err) {
+					console.log('error in deleting the post');
+					return res.redirect('back');
+				}
+				return res.redirect('back');
+			});
+		} else if (true) {
+			Post.findById(postId, function(err, post) {
+				if (err) {
+					console.log('post not found');
+				} else if (post.user == req.user.id) {
+					comment.remove();
+					Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, function(err, post) {
+						if (err) {
+							console.log('error in deleting the post');
+							return res.redirect('back');
+						}
+						return res.redirect('back');
+					});
+				}
+			});
+		} else {
+			return res.redirect('back');
+		}
+	});
+};
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,7 @@
 const Post = require('../modals/post');
 
+const Comment = require('../modals/comment');
+
 module.exports.create = function(req, res) {
 	Post.create(
 		{
@@ -14,4 +16,27 @@ module.exports.create = function(req, res) {
 			return res.redirect('back');
 		}
 	);
+};
+
+module.exports.destroy = function(req, res) {
+	Post.findById(req.params.id, function(err, post) {
+		if (err) {
+			console.log('error in finding the post');
+			res.redirect('/');
+		} else if (post.user == req.user.id) {
+			//.id means converting the object id to string
+			post.remove();
+			Comment.deleteMany(
+				{
+					post: req.params.id
+				},
+				function(err) {
+					return res.redirect('back');
+				}
+			);
+		} else {
+			console.log('User mismatch');
+			return res.redirect('back');
+		}
+	});
 };
