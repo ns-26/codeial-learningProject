c45a881a80b57319cfb33c4dcfff2ab357d432f3
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-12 13:26:21 +0530

    async await added everywhere

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,64 +1,51 @@
 const Comment = require('../modals/comment');
 
 const Post = require('../modals/post');
+const { response } = require('express');
 
-module.exports.create = function(req, res) {
-	Post.findById(req.body.post, function(err, post) {
-		if (err) {
-			console.log('Error in finding the post');
-		}
+module.exports.create = async function(req, res) {
+	try {
+		let post = await Post.findById(req.body.post);
 		if (post) {
-			Comment.create(
-				{
-					content: req.body.content,
-					user: req.user._id,
-					post: req.body.post
-				},
-				function(err, comment) {
-					if (err) {
-						console.log('error in creating a comment');
-					}
-					post.comments.push(comment);
-					post.save();
-					res.redirect('/');
-				}
-			);
+			let comment = await Comment.create({
+				content: req.body.content,
+				user: req.user._id,
+				post: req.body.post
+			});
+			post.comments.push(comment);
+			post.save();
+			res.redirect('/');
+		} else {
+			console.log('post not found');
+			return res.redirect('back');
 		}
-	});
+	} catch (err) {
+		console.log(err);
+		return res.redirect('back');
+	}
 };
 
-module.exports.destroy = function(req, res) {
-	Comment.findById(req.params.id, function(err, comment) {
+module.exports.destroy = async function(req, res) {
+	try {
+		let comment = await Comment.findById(req.params.id);
 		let postId = comment.post;
-		if (err) {
-			console.log('comment does not exist');
-			return res.redirect('back');
-		} else if (req.user.id == comment.user) {
+		if (req.user.id == comment.user) {
 			comment.remove();
-			Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, function(err, post) {
-				if (err) {
-					console.log('error in deleting the post');
-					return res.redirect('back');
-				}
-				return res.redirect('back');
-			});
-		} else if (true) {
-			Post.findById(postId, function(err, post) {
-				if (err) {
-					console.log('post not found');
-				} else if (post.user == req.user.id) {
-					comment.remove();
-					Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, function(err, post) {
-						if (err) {
-							console.log('error in deleting the post');
-							return res.redirect('back');
-						}
-						return res.redirect('back');
-					});
-				}
-			});
-		} else {
+			await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
 			return res.redirect('back');
+		} else if (true) {
+			let post = await Post.findById(postId);
+			if (post.user == req.user.id) {
+				comment.remove();
+				await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+				return res.redirect('back');
+			} else {
+				console.log('User not found');
+				return res.redirect('back');
+			}
 		}
-	});
+	} catch (err) {
+		console.log(err);
+		return res.redirect('back');
+	}
 };
