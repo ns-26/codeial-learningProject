c1afaf4ec981d592bc2fd84036211dd051c8b2ad
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-12 22:07:56 +0530

    data sent from form using ajax

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,13 +1,22 @@
 const Post = require('../modals/post');
 
 const Comment = require('../modals/comment');
+const { response } = require('express');
 
 module.exports.create = async function(req, res) {
 	try {
-		await Post.create({
+		let post = await Post.create({
 			content: req.body.content,
 			user: req.user._id
 		});
+		if (req.xhr) {
+			return res.status(200).json({
+				data: {
+					post: post
+				},
+				message: 'Post Created!'
+			});
+		}
 		req.flash('success', 'Post Published');
 		return res.redirect('back');
 	} catch (err) {
