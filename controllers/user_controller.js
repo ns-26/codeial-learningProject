51e3ac3150e98757b375532be9a63228b277b0db
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-15 22:50:03 +0530

    add code for deleting the user avatar from the file system

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,10 @@
 // import schema
 const User = require('../modals/user');
 
+const path = require('path');
+
+const fs = require('fs');
+
 //render the profile page
 module.exports.profile = function(req, res) {
 	User.findById(req.params.id, function(err, user) {
@@ -24,6 +28,12 @@ module.exports.update = async function(req, res) {
 				user.name = req.body.name;
 				user.email = req.body.email;
 				if (req.file) {
+					if (user.avatar) {
+						if (fs.existsSync(path.join(__dirname, '..', 'user.avatar'))) {
+							fs.unlinkSync(path.join(__dirname, '..', 'user.avatar'));
+						}
+					}
+
 					console.log(req.file);
 					//this is saving the path of the uploaded file into the avatar field of the user
 					user.avatar = User.avatarPath + '/' + req.file.filename;
