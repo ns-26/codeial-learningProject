2e3865aa083232de61c34f156461cad577dc0d70
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-16 20:28:55 +0530

    add code for deleting a post with api and showing relevant data in api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,20 +1,66 @@
 const Post = require('../../../modals/post');
 
+const Comment = require('../../../modals/comment');
+
 module.exports.index = async function(req, res) {
 	try {
-		let posts = await Post.find({}).sort('-createdAt').populate('user').populate({
-			path: 'comments',
-			populate: {
-				path: 'user'
-			}
-		});
+		let posts = await Post.find({})
+			.sort('-createdAt')
+			.populate({
+				path: 'user',
+				select: {
+					password: 0,
+					__v: 0
+				}
+			})
+			.select({
+				__v: 0
+			})
+			.populate({
+				path: 'comments',
+				select: {
+					__v: 0
+				},
+				populate: {
+					path: 'user',
+					select: {
+						password: 0,
+						__v: 0
+					}
+				}
+			});
+		// return res.send(data);
 		res.status(200).json({
 			message: 'lists of posts',
 			posts: posts
 		});
 	} catch (error) {
 		console.log(error);
-		req.status(500).json({
+		return res.status(500).json({
+			message: 'Internal Server Error'
+		});
+	}
+};
+
+module.exports.destroy = async function(req, res) {
+	try {
+		let post = await Post.findById(req.params.id);
+		// if (post.user == req.user.id) {
+		//.id means converting the object id to string
+		post.remove();
+		await Comment.deleteMany({
+			post: req.params.id
+		});
+		return res.status(200).json({
+			message: 'Post and associated Comments deleted successfully'
+		});
+		// } else {
+		// 	console.log('User mismatch');
+		// 	return res.redirect('back');
+		// }
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({
 			message: 'Internal Server Error'
 		});
 	}
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -6,4 +6,6 @@ const postsApi = require('../../../controllers/api/v1/posts_api');
 
 router.get('/', postsApi.index);
 
+router.delete('/delete/:id', postsApi.destroy);
+
 module.exports = router;
