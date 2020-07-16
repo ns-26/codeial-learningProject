d6d5861bde274b8b8a090b7baa6903baed084653
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-16 18:40:12 +0530

    add code for populating json objects in api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,21 @@
-module.exports.index = function(req, res) {
-	res.status(200).json({
-		message: 'lists of posts',
-		posts: []
-	});
+const Post = require('../../../modals/post');
+
+module.exports.index = async function(req, res) {
+	try {
+		let posts = await Post.find({}).sort('-createdAt').populate('user').populate({
+			path: 'comments',
+			populate: {
+				path: 'user'
+			}
+		});
+		res.status(200).json({
+			message: 'lists of posts',
+			posts: posts
+		});
+	} catch (error) {
+		console.log(error);
+		req.status(500).json({
+			message: 'Internal Server Error'
+		});
+	}
 };
