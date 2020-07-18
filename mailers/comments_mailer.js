cb9dc03eb2666bdab8d22a7de411310fae16e5eb
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-07-18 15:07:38 +0530

    add to for rendering a template of mails

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,13 +1,13 @@
 const nodeMailer = require('../config/nodemailer');
 //another way of exporting a method  other way was module.exports.newComment
 exports.newComment = (comment) => {
-	console.log('inside new Comment mailer', comment);
+	let htmlString = nodeMailer.renderTemplate({ comment: comment }, '/comments/new_comment.ejs');
 	nodeMailer.transporter.sendMail(
 		{
 			from: 'navdeepsingh.26.2000@gmail.com',
 			to: comment.user.email,
 			subject: 'New Comment Published',
-			html: '<h1>Comment Published</h1>'
+			html: htmlString
 		},
 		(err, info) => {
 			if (err) {
