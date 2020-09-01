48cce6a89b2a15891d381c3041ec93700f4ce9ae
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-09-02 00:22:43 +0530

    setup socket.io

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const express = require('express'),
-	app = express(),
-	port = 8000,
-	path = require('path'),
-	db = require('./config/mongoose'),
-	expressLayouts = require('express-ejs-layouts'),
-	cookieParser = require('cookie-parser'),
-	session = require('express-session'),
-	passport = require('passport'),
-	passportLocal = require('./config/passport_local_stratergy'),
-	passportJWT = require('./config/passport_jwt_strategy'),
-	passportGoogle = require('./config/passport-google-oauth2-strategy'),
-	mongoStore = require('connect-mongo')(session), //storing the session where the connect needs to work
-	sassMiddleware = require('node-sass-middleware'),
-	flash = require('connect-flash'),
-	customMware = require('./config/middleware');
+const express = require("express"),
+  app = express(),
+  port = 8000,
+  path = require("path"),
+  db = require("./config/mongoose"),
+  expressLayouts = require("express-ejs-layouts"),
+  cookieParser = require("cookie-parser"),
+  session = require("express-session"),
+  passport = require("passport"),
+  passportLocal = require("./config/passport_local_stratergy"),
+  passportJWT = require("./config/passport_jwt_strategy"),
+  passportGoogle = require("./config/passport-google-oauth2-strategy"),
+  mongoStore = require("connect-mongo")(session), //storing the session where the connect needs to work
+  sassMiddleware = require("node-sass-middleware"),
+  flash = require("connect-flash"),
+  customMware = require("./config/middleware");
+
+// socket config (chat server)
+const chatServer = require("http").Server(app);
+const chatSockets = require("./config/chat_sockets").chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("chat server is listening on port 5000");
 
 app.use(
-	sassMiddleware({
-		src: './assets/scss',
-		dest: './assets/css',
-		debug: true,
-		outputStyle: 'extended',
-		prefix: '/css'
-	})
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
+    debug: true,
+    outputStyle: "extended",
+    prefix: "/css",
+  })
 );
 
 //form controller
@@ -32,44 +38,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //setting up uploads
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 //static files
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 //express layout
 app.use(expressLayouts);
 //extract styles and scripts fron sub pages
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //mongostore is used to store the session cookie
 
 app.use(
-	session({
-		name: 'codeial',
-		//change the secret before before deployment in production
+  session({
+    name: "codeial",
+    //change the secret before before deployment in production
 
-		secret: 'blahsomething',
-		saveUninitialized: false,
-		resave: false,
-		cookie: {
-			maxAge: 1000 * 60 * 100
-		},
-		store: new mongoStore(
-			{
-				mongooseConnection: db,
-				autoReconnect: 'disabled'
-			},
-			function(err) {
-				console.log(err || 'connect mongodb setup ok');
-			}
-		)
-	})
+    secret: "blahsomething",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: new mongoStore(
+      {
+        mongooseConnection: db,
+        autoReconnect: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect mongodb setup ok");
+      }
+    ),
+  })
 );
 
 app.use(passport.initialize());
@@ -82,12 +88,12 @@ app.use(flash());
 app.use(customMware.setFlash);
 
 //use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 //port config
-app.listen(port, function(err) {
-	if (err) {
-		console.log(`Error in running the server:${err}`);
-	}
-	console.log(`Port is running on :${port}`);
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server:${err}`);
+  }
+  console.log(`Port is running on :${port}`);
 });
