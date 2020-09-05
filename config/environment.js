eddb33ca1364ca4227ae13f95e0d69a1cda52c49
ns-26 <56130142+ns-26@users.noreply.github.com>
2020-09-06 01:09:27 +0530

    environment variables done

--- config/environment.js
+++ config/environment.js
@@ -22,6 +22,27 @@ const development = {
 
 const production = {
   name: "production",
+  asset_path: "/assets",
+  session_cookie_key: "blahsomething",
+  db: "codeial_development",
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "navdeepsingh.26.2000@gmail.com",
+      pass: "",
+    },
+  },
+  google_client_id:
+    "658321949336-ag6vqmf8ss399bdk6rjoua4rbpif9gjo.apps.googleusercontent.com",
+  google_client_secret: "BVkKy73Xh5fl2-HhDaOLevjc",
+  google_callback_url: "http://localhost:3000/users/auth/google/callback",
+  jwt_secret: "codeial",
 };
 
-module.exports = development;
+module.exports =
+  eval(process.env.CODEIAL_ENVIRONMENT) === undefined
+    ? development
+    : eval(process.env.CODEIAL_ENVIRONMENT);
